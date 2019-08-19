a98900583802be60273de9b74352efedc5998a0d
Aria Bagas <aariabagass@gmail.com>
2019-08-19 23:24:02 +0700

    JS Task 2
    boolean, array, null, number

---
+++ tugas2.js
@@ -0,0 +1,23 @@
+var message = "Halo Dunia";
+
+var price = 90;
+if (price < 100)
+  discount = true;
+else
+  discount = false;
+
+var ruler = [5, 10, 15, 20, 25];
+
+var empty = "full";
+empty = null;
+var nothing =
+
+
+
+
+  console.log(price)
+console.log(message)
+console.log(discount)
+console.log(empty)
+console.log(nothing)
+console.log(ruler[1])
